db1994e91a82b1aa0beb94e3ddb63e122b8424ee
João P. Meneguesso <jpm83633@gmail.com>
2022-12-12 19:31:39 -0300

    inicio - HELLO WORLD - NPM

---
+++ node/aula1/index.js
@@ -0,0 +1,24 @@
+console.log("hello world")
+
+/* 
+não existe no node o document e window
+
+console.log(document)
+console.log(window)
+*/
+
+// o node tem alguns metodos que o browser não tem
+// é um ambiente javascript normal
+
+function teste(){
+    console.log('teste')
+}
+
+teste()
+
+
+
+//pesquisar npm 
+//bus-promise (biblioteca do professor), moongose etc
+
+//npm --> onde está hospedado todos os pacotes do node
