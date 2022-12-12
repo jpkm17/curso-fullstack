8347997f30f2fa21b7e6d9d0435468a3e89a2638
João P. Meneguesso <jpm83633@gmail.com>
2022-12-12 19:51:04 -0300

    Principais comandos e package JSON

---
+++ node/aula2/index.js
@@ -0,0 +1,13 @@
+/*  
+  npm init: inicia uma aplicação em node.js
+  package.json: coração da aplicação
+  npm install nomeDoPacote: faz a instalação do pacote
+  package-lock: arquivo de controle do node
+  npm i nodemom --save-dev: cria uma nova dependenci no package.json
+  npm uninstall nodemon: desinstala o pacote
+*/  
+
+/* 
+dependencies: pacotes crucias para o funcionamento do projeto
+devDependencies: pacotes que não são importantes para o funcionamento do projeto
+*/
