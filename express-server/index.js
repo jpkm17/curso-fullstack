719523254996ec2717db6664431e86fbdc5033ec
João P. Meneguesso <jpm83633@gmail.com>
2022-12-13 12:24:20 -0300

    Criando rotas

--- express-server/index.js
+++ express-server/index.js
@@ -1,8 +1,23 @@
-// npm i express
 const express = require('express')
-
-//express é uma function
 const app = express()
 
+// req --> request, res --> response
+// Isso é uma rota
+app.get('/', (req, res)=>{
+    res.send('hello world')
+})
+
+
+app.get('/sobre', (req, res)=>{
+    res.send('Sobre')
+})
+
+// 404 error (not found)
+app.use((req, res)=>{ // middleware
+    res.send('pagina não encontrada')
+})
+
+
+
 const port = process.env.PORT || 8080
-app.listen(port, ()=> console.log(`Server is listen on ${port}`))
+app.listen(port, ()=> console.log(`Server is listen on port ${port}`))
